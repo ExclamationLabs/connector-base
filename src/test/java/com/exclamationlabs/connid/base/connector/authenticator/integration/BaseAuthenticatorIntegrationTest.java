3a20cb7782bb2e27fea2ef4883e04a280b05e8cb
Mike Neugebauer <mneugebauer@exclamationlabs.com>
2020-06-16 12:40:22 -0500

    #CU-74cjb0 - bring in test support dependency jar, added copyrights

--- src/test/java/com/exclamationlabs/connid/base/connector/authenticator/integration/BaseAuthenticatorIntegrationTest.java
+++ src/test/java/com/exclamationlabs/connid/base/connector/authenticator/integration/BaseAuthenticatorIntegrationTest.java
@@ -1,9 +1,25 @@
+/*
+    Copyright 2020 Exclamation Labs
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
 package com.exclamationlabs.connid.base.connector.authenticator.integration;
 
-import com.exclamationlabs.connid.base.connector.IntegrationTest;
 import com.exclamationlabs.connid.base.connector.configuration.BaseConnectorConfiguration;
 import com.exclamationlabs.connid.base.connector.configuration.ConnectorProperty;
 import com.exclamationlabs.connid.base.connector.configuration.TestConnectorConfiguration;
+import com.exclamationlabs.connid.base.connector.test.IntegrationTest;
 import org.identityconnectors.common.logging.Log;
 import org.identityconnectors.framework.common.exceptions.ConfigurationException;
 import org.junit.Assume;
