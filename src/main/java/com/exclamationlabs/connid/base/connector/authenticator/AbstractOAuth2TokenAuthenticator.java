df8923685322b533a99c2603cec3cf33fa1b153e
Mike Neugebauer <mneugebauer@exclamationlabs.com>
2020-07-15 13:52:40 -0500

    CU-7weuw2 - add additional headers support to Authenticator (for RingCentral)

--- src/main/java/com/exclamationlabs/connid/base/connector/authenticator/AbstractOAuth2TokenAuthenticator.java
+++ src/main/java/com/exclamationlabs/connid/base/connector/authenticator/AbstractOAuth2TokenAuthenticator.java
@@ -39,8 +39,9 @@ public abstract class AbstractOAuth2TokenAuthenticator implements Authenticator 
 
     private static final Log LOG = Log.getLog(AbstractOAuth2TokenAuthenticator.class);
 
-    protected static String executeRequest(BaseConnectorConfiguration configuration, HttpClient client, HttpPost request, UrlEncodedFormEntity entity, GsonBuilder gsonBuilder) throws IOException {
+    protected String executeRequest(BaseConnectorConfiguration configuration, HttpClient client, HttpPost request, UrlEncodedFormEntity entity, GsonBuilder gsonBuilder) throws IOException {
         request.setEntity(entity);
+        getAdditionalAuthenticationHeaders(configuration).forEach(request::setHeader);
         HttpResponse response = client.execute(request);
         int statusCode = response.getStatusLine().getStatusCode();
         LOG.info("OAuth2 Received {0} response for {1} {2}", statusCode,
